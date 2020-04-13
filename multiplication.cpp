bf18876aa42aed9b9fbf983ca620d4ccb1352da2
Meghashree541 <63413806+Meghashree541@users.noreply.github.com>
2020-04-13 16:57:42 +0530

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,c;
+    cout<<"enter 2 numbers:"<<endl;
+    cin>>a>>b;
+    c=a*b;
+    cout<<"Product is:"<<c<<endl;
+    return 0;
+}
